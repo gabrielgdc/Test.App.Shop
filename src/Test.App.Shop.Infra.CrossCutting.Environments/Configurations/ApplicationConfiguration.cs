e6e0322ba7cb32afcba68ff23723d62ad31770cd
Gabriel Guedes <gabrielgdc15@gmail.com>
2022-05-25 22:38:43 -0300

    add database configuration

--- src/Test.App.Shop.Infra.CrossCutting.Environments/Configurations/ApplicationConfiguration.cs
+++ src/Test.App.Shop.Infra.CrossCutting.Environments/Configurations/ApplicationConfiguration.cs
@@ -3,6 +3,7 @@
 public class ApplicationConfiguration
 {
     public string? Environment { get; set; }
+    public string? ConnectionString { get; set; }
     public string? GlobalErrorCode { get; set; }
     public string? GlobalErrorMessage { get; set; }
 }
--- src/Test.App.Shop.Infra.Data/Context/ApplicationDbContext.cs
+++ src/Test.App.Shop.Infra.Data/Context/ApplicationDbContext.cs
@@ -3,33 +3,32 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Test.App.Shop.Infra.CrossCutting.Environments.Configurations;
 
 namespace Test.App.Shop.Infra.Data.Context;
 
 public class ApplicationDbContext : DbContext
 {
     private readonly IMediator _bus;
+    private readonly ApplicationConfiguration _applicationConfiguration;
 
-    public ApplicationDbContext()
+    public ApplicationDbContext(ApplicationConfiguration applicationConfiguration)
     {
+        _applicationConfiguration = applicationConfiguration;
     }
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ApplicationConfiguration applicationConfiguration) : base(options)
     {
+        _applicationConfiguration = applicationConfiguration;
     }
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IMediator mediator) : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IMediator mediator, ApplicationConfiguration applicationConfiguration) : base(options)
     {
         _bus = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _applicationConfiguration = applicationConfiguration;
     }
 
-    // protected override void OnConfiguring(DbContextOptionsBuilder options) =>
-    // 	options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString"),
-    // 		npgsqlOptionsAction: pgOptions =>
-    // 		{
-    // 			pgOptions.EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(30), errorCodesToAdd: null);
-    // 		}
-    // );
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(_applicationConfiguration.ConnectionString ?? string.Empty);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -38,15 +37,15 @@ public class ApplicationDbContext : DbContext
 
     public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
     {
-        // Dispatch Domain Events collection. 
+        // Dispatch Domain Events collection.
         // Choices:
-        // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+        // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including
         // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
-        // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
-        // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
+        // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions.
+        // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers.
         await _bus.DispatchDomainEventsAsync(this);
 
-        // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
+        // After executing this line all the changes (from the Command Handler and Domain Event Handlers)
         // performed through the DbContext will be committed
         return await base.SaveChangesAsync(cancellationToken) > 0;
     }
